0af6077b99cdef920c9e330696a6c6f164cde203
LudmilKirov <ludmil9595@gmail.com>
2019-07-09 12:18:33 +0300

    Add activity log,and when rotate the screen to keep the info

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -6,31 +6,67 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG="QuizActivity";
+    private static final String KEY_INDEX="index";
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
-    private Button mPrevButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
     private TextView mQuestionTextView;
     private int mCurrentIndex = 0;
+
     //Make a array of questions
     private Question[] mQuestionBank = new Question[]{new Question(R.string.question_oceans, true),
             new Question(R.string.question_mideast, false),
             new Question(R.string.question_africa, false),
             new Question(R.string.question_america, true),
             new Question(R.string.question_asia, true)};
-
+    //State for Start
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart() called");
+    }
+    //Stae for Pause
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause() called");
+    }
+    //State for Resume
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume() called");
+    }
+    //State for Stop
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop() called");
+    }
+    //State for Destroy
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy() called");
+    }
+    //State for Create
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate() called");
         setContentView(R.layout.activity_main);
-
+        //Make when clicking the text to change to next question
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+        mQuestionTextView.
+                setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
@@ -55,7 +91,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         //Add a next button
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton =  findViewById(R.id.next_button);
+
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -63,8 +100,9 @@ public class MainActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+
         //Add a prev button
-        mPrevButton=(Button)findViewById(R.id.prev_button);
+        mPrevButton=findViewById(R.id.prev_button);
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -72,6 +110,19 @@ public class MainActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+        //Check the state of the program
+        if(savedInstanceState!=null){
+            mCurrentIndex=savedInstanceState.getInt(KEY_INDEX,0);
+        }
+        updateQuestion();
+    }
+    //Save the state of the program,when
+    // rotate the screen to save the information
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG,"onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX,mCurrentIndex);
     }
 
     //Update question method
