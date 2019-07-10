8ec87566c68f669b581dbab74239b5b8409bd50c
LudmilKirov <ludmil9595@gmail.com>
2019-07-10 14:30:49 +0300

    Rotate fix,add a cheat counter and get answered a cheated question

--- app/src/main/java/com/example/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/geoquiz/CheatActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,6 +15,11 @@ public class CheatActivity extends AppCompatActivity {
             "com.example.qeoquiz.answer_is_true";
     private static final String EXTRA_ANSWER_SHOWN=
             "com.example.qeoquiz.answer_shown";
+
+    private static final String TAG = "CheatActivity";
+    private static final String KEY_CHEAT_CHECKER = "cheat_checker";
+    private static final String KEY_ANSWER = "answer_to_question";
+    private boolean mIsAnswerShown;
     private boolean mAnswerIsTrue;
     private TextView mAnswerTextView;
     private Button mShowAnswer;
@@ -23,9 +29,18 @@ public class CheatActivity extends AppCompatActivity {
         i.putExtra(EXTRA_ANSWER_IS_TRUE,answerIsTrue);
         return i;
     }
+
     //Set default value for the cheat button
     public  static boolean wasAnswerShown(Intent result){
         return result.getBooleanExtra(EXTRA_ANSWER_SHOWN,false);
+    }
+
+    //Set the answer is true when
+    // the user presses the show answer button
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent data=new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN,isAnswerShown);
+        setResult(RESULT_OK,data);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,32 +49,47 @@ public class CheatActivity extends AppCompatActivity {
 
         mAnswerIsTrue=getIntent()
                 .getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false);
-
         mAnswerTextView=(TextView)findViewById(R.id.answer_text_view);
-
         mShowAnswer=(Button)findViewById(R.id.show_answer_button);
         mShowAnswer.setOnClickListener(new View.OnClickListener() {
-
             @Override
-            public void onClick(View view) {
-                if(mAnswerIsTrue){
-                    mAnswerTextView.setText(R.string.true_button);
-                }
-                else{
-                    mAnswerTextView.setText(R.string.false_button);
-                }
+            public void onClick(View v) {
+                displayAnswer();
                 setAnswerShownResult(true);
+                mIsAnswerShown = true;
             }
         });
+        //If it is not null remember the state if the answer is shown
+        if (savedInstanceState!=null){
+            mIsAnswerShown = savedInstanceState
+                    .getBoolean(KEY_CHEAT_CHECKER, false);
+            mAnswerIsTrue = savedInstanceState
+                    .getBoolean(KEY_ANSWER, false);
+            if(mIsAnswerShown){
+                setAnswerShownResult(true);
+                displayAnswer();
+            }
+        }
+    }
 
+    //Show the answer
+    public void displayAnswer(){
+        if(mAnswerIsTrue){
+            mAnswerTextView.setText(R.string.true_button);
+        }
+        else{
+            mAnswerTextView.setText(R.string.false_button);
+        }
     }
-    //Set the answer is true when
-    // the user presses the show answer button
-    private void setAnswerShownResult(boolean isAnswerShown) {
-        Intent data=new Intent();
-        data.putExtra(EXTRA_ANSWER_SHOWN,isAnswerShown);
-        setResult(RESULT_OK,data);
+
+    //To save the state if the answer is shown and if the answer is true
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "in onSavedInstanceState");
+        savedInstanceState.putBoolean(KEY_CHEAT_CHECKER, mIsAnswerShown);
+        savedInstanceState.putBoolean(KEY_ANSWER, mAnswerIsTrue);
     }
-    //gg
-    String gg="";
+
+
 }
--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -14,182 +15,262 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG="QuizActivity";
-    private static final String KEY_INDEX="index";
-    private static final int REQUEST_CODE_CHEAT=0;
+    private static final String TAG = "QuizActiviy";
+    private static final String KEY_INDEX = "index";
+    private static final String KEY_SCORE = "score";
+    private static final String KEY_CHEATER = "cheater";
+    private static final String KEY_TOKENS = "tokens";
+    private static final int REQUEST_CODE_CHEAT = 0;
+
     private Button mTrueButton;
     private Button mFalseButton;
-    private ImageButton mNextButton;
-    private ImageButton mPrevButton;
     private Button mCheatButton;
+    private ImageButton mNextButton;
+    private ImageButton mPreviousButton;
     private TextView mQuestionTextView;
-    private int mCurrentIndex = 0;
-    private boolean mIsCheater;
+    private TextView mRemainingTokensTextView;
 
-    //Make a array of questions
-    private Question[] mQuestionBank = new Question[]{
+    private Question[] mQuestionBank = new Question[] {
             new Question(R.string.question_oceans, true),
             new Question(R.string.question_mideast, false),
             new Question(R.string.question_africa, false),
             new Question(R.string.question_america, true),
-            new Question(R.string.question_asia, true)};
-    //State for Start
-    @Override
-    public void onStart(){
-        super.onStart();
-        Log.d(TAG,"onStart() called");
-    }
-    //Stae for Pause
-    @Override
-    public void onPause(){
-        super.onPause();
-        Log.d(TAG,"onPause() called");
-    }
-    //State for Resume
-    @Override
-    public void onResume(){
-        super.onResume();
-        Log.d(TAG,"onResume() called");
-    }
-    //State for Stop
-    @Override
-    public void onStop(){
-        super.onStop();
-        Log.d(TAG,"onStop() called");
-    }
-    //State for Destroy
-    @Override
-    public void onDestroy(){
-        super.onDestroy();
-        Log.d(TAG,"onDestroy() called");
-    }
-    private void checkAnswer(boolean userPressedTrue) {
-        //Check if the answer is true
-        //If it is true pop a correct toast else,incorrect
-        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
-        int messageResId = 0;
-        if (mIsCheater) {
-            messageResId = R.string.judgement_toast;
-        }
-        else {
-            if (userPressedTrue == answerIsTrue) {
-                messageResId = R.string.correct_toast;
-            } else {
-                messageResId = R.string.incorrect_toast;
-            }
-        }
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
-    }
-    //State for Create
+            new Question(R.string.question_asia, true),
+    };
+
+    private boolean[] mIsCheater = new boolean[mQuestionBank.length];
+    private int mCurrentIndex = 0;
+    private int mCurrentScore = 0;
+    //You can use max 3 cheat tokens
+    private int mRemainingCheatTokens = 3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG,"onCreate() called");
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
 
-        //Make when clicking the text to change to next question
-        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-        mQuestionTextView.
-                setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                mIsCheater=false;
-                updateQuestion();
-            }
-        });
-
-        mTrueButton = (Button) findViewById(R.id.true_button);
-        //When clicked show incorrect toast
-        mTrueButton
-                .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                checkAnswer(true);
-            }
-        });
-
-        mFalseButton = (Button) findViewById(R.id.false_button);
-        //When clicked show correct toast
-
-        mFalseButton
-                .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                checkAnswer(false);
-            }
-        });
-
-        mNextButton =  findViewById(R.id.next_button);
-        //Add a next button
-        mNextButton
-                .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                updateQuestion();
-            }
-        });
-
-        //Add a prev button
-        mPrevButton=findViewById(R.id.prev_button);
-        mPrevButton
-                .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                updateQuestion();
-            }
-        });
-
-        //Create a cheat button
-        mCheatButton=(Button)findViewById(R.id.cheat_button);
-        mCheatButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                //Start Cheat Activity
-               boolean answerIsTRue=mQuestionBank[mCurrentIndex].isAnswerTrue();
-               Intent i =CheatActivity
-                       .newIntent(MainActivity.this,answerIsTRue);
-                startActivityForResult(i,REQUEST_CODE_CHEAT);
-            }
-        });
-        //Check the state of the program
-        if(savedInstanceState!=null){
-            mCurrentIndex=savedInstanceState.getInt(KEY_INDEX,0);
+        if (savedInstanceState != null) {
+            //Save the index of the question
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            //Save the right answered questions
+            mCurrentScore = savedInstanceState.getInt(KEY_SCORE, 0);
+            //Save how many cheat tokens left
+            mRemainingCheatTokens = savedInstanceState.getInt(KEY_TOKENS, 3);
+            //Save if on the question is used a cheat
+            mIsCheater = savedInstanceState.getBooleanArray(KEY_CHEATER);
         }
+
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View l) {
+                MainActivity.this.nextQuestion();
+            }
+        });
+        //Label the cheat button with a text and remaining tokens
+        mRemainingTokensTextView = findViewById(R.id.cheat_button);
+        mRemainingTokensTextView.setText
+                ("Remaining Cheat Tokens: " + mRemainingCheatTokens);
+
+        //True button
+        mTrueButton = findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View l) {
+                MainActivity.this.checkAnswer(true);
+            }
+        });
+
+        //False button
+        mFalseButton = findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View l) {
+                MainActivity.this.checkAnswer(false);
+            }
+        });
+
+        //Next button
+        mNextButton = findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View l) {
+                MainActivity.this.nextQuestion();
+            }
+        });
+
+        //Previous button
+        mPreviousButton = findViewById(R.id.prev_button);
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View l) {
+                MainActivity.this.previousQuestion();
+            }
+        });
+
+        //Cheat button
+        mCheatButton = findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Start CheatActivity
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent intent = CheatActivity.newIntent(MainActivity.this, answerIsTrue);
+                MainActivity.this.startActivityForResult(intent, REQUEST_CODE_CHEAT);
+            }
+        });
+
         updateQuestion();
+
     }
 
     @Override
-    protected void onActivityResult(int requestCode,int resultCode,Intent data){
-        if(resultCode!= Activity.RESULT_OK){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
             return;
         }
-        if(requestCode==REQUEST_CODE_CHEAT){
-            //Check if it is empty
-            if(data==null){
+
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            if (data == null) {
                 return;
             }
-            mIsCheater=CheatActivity.wasAnswerShown(data);
+            mIsCheater[mCurrentIndex] = CheatActivity.wasAnswerShown(data);
+            //Limited to 3 cheats
+            mRemainingCheatTokens--;
+            //Update the label
+            mRemainingTokensTextView.setText
+                    ("Remaining Cheat Tokens: " + mRemainingCheatTokens);
+            if (mRemainingCheatTokens == 0) {
+                //If it goes to 0 unable the cheat button
+                mCheatButton.setEnabled(false);
+            }
         }
     }
-    //Save the state of the program,when
-    // rotate the screen to save the information
+
+
+    //On save instance,that remember the current question,
+    // the answered question,remaining cheat tokens
+    // and if the cheat is used on the question
     @Override
-    public void onSaveInstanceState(Bundle savedInstanceState){
+    public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
-        Log.i(TAG,"onSaveInstanceState");
-        savedInstanceState.putInt(KEY_INDEX,mCurrentIndex);
+        Log.i(TAG, "onSaveInstanceState() called");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+        savedInstanceState.putInt(KEY_SCORE, mCurrentScore);
+        savedInstanceState.putInt(KEY_TOKENS, mRemainingCheatTokens);
+        savedInstanceState.putBooleanArray(KEY_CHEATER, mIsCheater);
     }
 
+    //For previous question
+    private void previousQuestion() {
+        if (mCurrentIndex != 0) {
+            mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+        } else {
+            mCurrentIndex = mQuestionBank.length - 1;
+        }
+
+        updateQuestion();
+    }
+    //For next question and to restart the score
+    private void nextQuestion() {
+        mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+
+        updateQuestion();
+
+        //Restart the score when start over
+        if (mCurrentIndex == 0) {
+            mCurrentScore = 0;
+        }
+    }
     //Update question method
     private void updateQuestion() {
-//        Log.d(TAG,"Updation question text for question #"
-//                        +mCurrentIndex,new Exception());
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
+        toggleAnswerButtonsTo(true);
     }
 
+    private void checkAnswer(boolean userPressedTrue) {
 
+        toggleAnswerButtonsTo(false);
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId;
+
+        if (mIsCheater[mCurrentIndex]) {
+            //Pop the cheating toast
+            messageResId = R.string.judgement_toast;
+            //Cheaters doesn't score
+        }
+        else {
+            if (userPressedTrue == answerIsTrue) {
+                //Pop the correct toast
+                messageResId = R.string.correct_toast;
+                //Update the right answer that are not cheated
+                mCurrentScore += 1;
+                Log.d(TAG, mCurrentScore + "");
+            }
+            else {
+                //Pop the incorrect toast
+                messageResId = R.string.incorrect_toast;
+            }
+        }
+        //Toast for correctness
+        Toast toast = Toast.makeText(this, messageResId, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.BOTTOM, 0, 20);
+        toast.show();
+        //IF reach the final question
+        if (mCurrentIndex == mQuestionBank.length - 1) {
+            showScore();
+        }
+    }
+
+    private void showScore() {
+        //Calculate the percentage
+        int percentage = (int) (((double)mCurrentScore/mQuestionBank.length)*100);
+        //String for the toast
+        String stringScore = "You got " + percentage + "% correct answers";
+        //Pop the toast
+        Toast.makeText(this, stringScore, Toast.LENGTH_SHORT).show();
+    }
+
+    //To stop the ability of the user to answer
+    private void toggleAnswerButtonsTo(boolean b) {
+        if (!b) {
+            mTrueButton.setEnabled(false);
+            mFalseButton.setEnabled(false);
+        }
+        else  {
+            mTrueButton.setEnabled(true);
+            mFalseButton.setEnabled(true);
+        }
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
 }
